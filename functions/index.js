0df82c2544118ccfa740bfb9ce6a3603d4da4048
Manish Bisht <manish.bisht490@gmail.com>
2017-06-25 12:34:27 +0530

    function updated ... fetching contents now

--- functions/index.js
+++ functions/index.js
@@ -9,21 +9,46 @@ var MetaInspector = require('node-metainspector');
 exports.getlinkmetadata = functions.database.ref('/links/{pushId}/link')
 	.onWrite(event => {
 	// Grab the current value of what was written to the Realtime Database.
-	const original = event.data.val();
-	var client = new MetaInspector(original, { timeout: 10000 });
+	const url = event.data.val();
+	var client = new MetaInspector(url, { timeout: 10000 });
 	client.on("fetch", function(){
-		event.data.ref.parent.child('title').set(client.title);
-		event.data.ref.parent.child('description').set(client.description);
-		console.log(client.title);
+		if (client.ogTitle) {
+			event.data.ref.parent.child('title').set(client.ogTitle);
+		}
+		else if (client.title) {
+			event.data.ref.parent.child('title').set(client.title);
+		}
+		else {
+			event.data.ref.parent.child('title').set("No Page Title Found");
+		}
+		if (client.ogDescription) {
+			event.data.ref.parent.child('description').set(client.ogDescription);
+		}
+		else if (client.description) {
+			event.data.ref.parent.child('description').set(client.description);
+		}
+		else {
+			event.data.ref.parent.child('description').set("No Description Found");
+		}
+		if (client.image) {
+			event.data.ref.parent.child('image').set(client.image);
+		}
+		else {
+			event.data.ref.parent.child('image').set("http://www.worldbank.org/content/dam/wbr/previewnotavailable.gif");
+		}
 	});
 	client.on("error", function(err) {
-		console.log(err);
+		if(!err) {
+			event.data.ref.parent.child('title').set("No Page Title Found");
+			event.data.ref.parent.child('description').set("No Description Found");
+			event.data.ref.parent.child('image').set("http://www.worldbank.org/content/dam/wbr/previewnotavailable.gif");
+		}
 	});
 	client.fetch();
-	console.log('Uppercasing', event.params.pushId, original);
-	const uppercase = original.toUpperCase();
+	//console.log('Uppercasing', event.params.pushId, url);
 	// You must return a Promise when performing asynchronous tasks inside a Functions such as
 	// writing to the Firebase Realtime Database.
 	// Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-	return event.data.ref.parent.child('uppercase').set(uppercase);
+	var d = (new Date()).toGMTString();
+	return event.data.ref.parent.child('created').set(d);
 });
